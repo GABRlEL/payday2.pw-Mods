a21f0c94dfc5fda4db046ca1ec01f572b5da5174
Gabriel <gab@saru.ch>
2018-08-22 18:44:06 +0200

    Fixed Free Infamy Skills
    Fixed a crash

--- Free Infamy Skills - Gab/code.lua
+++ Free Infamy Skills - Gab/code.lua
@@ -2,6 +2,9 @@ local old_IT_init = InfamyTweakData.init
 
 function InfamyTweakData:init(tweak_data)
 old_IT_init(self, tweak_data)
+local function digest(value)
+		return Application:digest_value(value, true)
+	end
 self.cost = {
 		root = digest(0),
 		tier1 = digest(0)
