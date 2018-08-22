cc1f86341b7c21e40fc6d8a52eb36510d6caf33c
Gabriel <gab@saru.ch>
2018-08-22 04:06:12 +0200

    Fixed Money Increaser
    Recoded Money Increaser, sell a weapon to get cash

--- Money Increaser - Gab/setup.lua
+++
@@ -1,6 +0,0 @@
-local old_UTD188_init = UpgradesTweakData._init_pd2_values
-
-function UpgradesTweakData:_init_pd2_values()
-    old_UTD188_init(self, tweak_data, team)
-	self.values.team.cash.stealth_money_multiplier = {1000000}
-end
