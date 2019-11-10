073fb263e43360fb6222a44035fc391168417922
Gabriel <gab@saru.ch>
2019-11-10 23:51:10 +0100

    Fixed Free Offshore PAYDAY
    Thanks to Chaos

--- Free Offshore PAYDAY - Gab/code.lua
+++ Free Offshore PAYDAY - Gab/code.lua
@@ -6,3 +6,21 @@ function MoneyManager:on_buy_casino_fee(secured_cards, increase_infamous, prefer
 	local amount = 0
 	return amount
 end
+
+function MoneyManager:get_cost_of_casino_fee(secured_cards, increase_infamous, preferred_card)
+	local fee = self:get_cost_of_casino_entrance()
+
+	for i = 1, secured_cards, 1 do
+		fee = 0
+	end
+
+	if increase_infamous then
+		fee = 0
+	end
+
+	if preferred_card and preferred_card ~= "none" then
+		fee = 0
+	end
+
+	return fee
+end
