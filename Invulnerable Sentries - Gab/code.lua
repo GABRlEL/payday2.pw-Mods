59f3c4ef0a9d9e0e0e668894425795da5ed95895
Gabriel <26207711+GABRlEL@users.noreply.github.com>
2022-08-06 21:20:32 +0200

    Small bug fixes
    Fixed/Improved Invulnerable sentries based on user complaints
    
    Fixed game-breaking bug occuring with No weather
    Known issue but won't fix: No weather only nerfs weather in some levels

--- Invulnerable Sentries - Gab/code.lua
+++ Invulnerable Sentries - Gab/code.lua
@@ -3,5 +3,7 @@ local old_CMIS_init = UpgradesTweakData._init_pd2_values
 function UpgradesTweakData:_init_pd2_values(tweak_data)
     old_CMIS_init(self, tweak_data)
 	self.sentry_gun_base_armor = 999999
+	self.values.sentry_gun.armor_multiplier2 = 9999999
+	self.values.sentry_gun.armor_multiplier = 9999999
 	end
 	
--- No weather - Gab/code.lua
+++ No weather - Gab/code.lua
@@ -1,23 +1,8 @@
-local old_noweather_init = EnvEffectTweakData.init
+function RainEffect:start()
+end
 
-function EnvEffectTweakData:init(tweak_data)
-old_noweather_init(self, tweak_data)
-	self.lightning = {
-		event_name = ""
-	}
-	self.lightning_tag = {
-		event_name = ""
-	}
-	self.rain = {
-		effect_name = ""
-	}
-	self.rain_only = {
-		effect_name = ""
-	}
-	self.snow = {
-		effect_name = ""
-	}
-	self.snow_slow = {
-		effect_name = ""
-	}
+function LightningEffect:start()
+end
+
+function RainDropEffect:start()
 end
